fffe489741ecdabb5fbc8222586fe667758e15d2
Kylin <125571361@qq.com>
2016-11-16 23:19:31 -0800

    Update MapNodeCompareTotal.java

--- UCSDGraphs/src/roadgraph/MapNodeCompareTotal.java
+++ UCSDGraphs/src/roadgraph/MapNodeCompareTotal.java
@@ -7,9 +7,9 @@ public class MapNodeCompareTotal implements Comparator<MapNode> {
 	@Override
 	public int compare(MapNode n1, MapNode n2) {
 		// TODO Auto-generated method stub
-		if(n1.getPreDis() > n2.getPreDis())
+		if(n1.getTotalDis() > n2.getTotalDis())
 			return 1;
-		else if(n1.getPreDis() < n2.getPreDis())
+		else if(n1.getTotalDis() < n2.getTotalDis())
 			return -1;
 		else
 			return 0;
